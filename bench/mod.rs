3b9c93ec6f45cbfc821204b600b50645a9f1ff78
Hao Shi <shisoftgenius@gmail.com>
2021-03-17 19:42:42 -0400

    adjust to fair workload

--- bench/mod.rs
+++ bench/mod.rs
@@ -52,15 +52,15 @@ where
     println!("Testing {}", name);
     println!("Read heavy");
 
-    let read_measure_75 = run_and_measure_mix::<T>(Mix::read_heavy(), 0.75, 25);
+    let read_measure_75 = run_and_measure_mix::<T>(Mix::read_heavy(), 0.75, 24);
     write_measures(&format!("{}_{}_75_read.csv", task, name), &read_measure_75);
 
-    let read_measure_150 = run_and_measure_mix::<T>(Mix::read_heavy(), 55.0, 20);
+    let read_measure_150 = run_and_measure_mix::<T>(Mix::read_heavy(), 55.0, 24);
     write_measures(&format!("{}_{}_150_read.csv", task, name), &read_measure_150);
 
 
     println!("Insert heavy");
-    let insert_measure_75 = run_and_measure_mix::<T>(Mix::insert_heavy(), 0.75, 25);
+    let insert_measure_75 = run_and_measure_mix::<T>(Mix::insert_heavy(), 0.75, 24);
     write_measures(&format!("{}_{}_75_insertion.csv", task, name), &insert_measure_75);
 
     let insert_measure_150 = run_and_measure_mix::<T>(Mix::insert_heavy(), 1.5, 24);
@@ -68,10 +68,10 @@ where
 
 
     println!("Uniform");
-    let uniform_measure_75 = run_and_measure_mix::<T>(Mix::uniform(), 0.75, 25);
+    let uniform_measure_75 = run_and_measure_mix::<T>(Mix::uniform(), 0.75, 24);
     write_measures(&format!("{}_{}_75_uniform.csv", task, name), &uniform_measure_75);
 
-    let uniform_measure_150 = run_and_measure_mix::<T>(Mix::uniform(), 6.0, 22);
+    let uniform_measure_150 = run_and_measure_mix::<T>(Mix::uniform(), 6.0, 24);
     write_measures(&format!("{}_{}_150_uniform.csv", task, name), &uniform_measure_150);
 }
 
