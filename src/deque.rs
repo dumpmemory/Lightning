2a3271af3a0d7c45f7aa91b568807774509c2374
Hao Shi <shisoftgenius@gmail.com>
2022-01-08 06:37:21 -0500

    all functions ready for testing

--- src/deque.rs
+++ src/deque.rs
@@ -4,7 +4,7 @@
 
 use std::{
     mem,
-    sync::atomic::{AtomicUsize, Ordering::*},
+    sync::atomic::{fence, AtomicUsize, Ordering::*},
 };
 
 use crossbeam_epoch::{self as epoch, Atomic, Owned, Shared};
@@ -41,15 +41,14 @@ impl<T: Clone> Deque<T> {
         Self { head, tail }
     }
 
-    pub fn insert_front(&mut self, value: T) {
+    pub fn insert_front(&self, value: T, guard: &Guard) {
         let backoff = crossbeam_utils::Backoff::new();
-        let guard = crossbeam_epoch::pin();
         let curr = Owned::new(Node::new(value)).into_shared(&guard);
-        let prev = self.head.load(Acquire, &guard);
+        let prev = self.head.load(Relaxed, &guard);
+        let prev_node = unsafe { prev.deref() };
         let mut next = unsafe { prev.deref().next.load(Acquire, &guard) };
         let curr_node = unsafe { curr.deref() };
-        loop { // Find the prev node
-            let (prev_node, prev_tag) = decomp_ptr(&prev);
+        loop {
             {
                 let prev_next = prev_node.next.load(Acquire, &guard);
                 if decomp_with_ptr(&prev_next) != (&next, EXISTED_TAG) {
@@ -62,13 +61,55 @@ impl<T: Clone> Deque<T> {
             curr_node.next.store(next.with_tag(EXISTED_TAG), Relaxed);
             if prev_node
                 .next
-                .compare_exchange(next.with_tag(EXISTED_TAG), curr, AcqRel, Acquire, &guard)
+                .compare_exchange(
+                    next.with_tag(EXISTED_TAG),
+                    curr.with_tag(EXISTED_TAG),
+                    AcqRel,
+                    Acquire,
+                    &guard,
+                )
+                .is_ok()
+            {
+                break;
+            }
+            backoff.spin();
+        } // I
+        fence(SeqCst);
+        Self::insert_next_ops(&curr, &next, &guard, &backoff)
+    }
+
+    pub fn insert_back(&self, value: T, guard: &Guard) {
+        let backoff = crossbeam_utils::Backoff::new();
+        let curr = Owned::new(Node::new(value)).into_shared(&guard);
+        let next = self.tail.load(Relaxed, &guard);
+        let curr_node = unsafe { curr.deref() };
+        loop {
+            let prev = unsafe { next.deref().prev.load(Acquire, &guard) };
+            let prev_node = unsafe { prev.deref() };
+            let prev_next = prev_node.next.load(Acquire, &guard);
+            if decomp_with_ptr(&prev_next) != (&next, EXISTED_TAG) {
+                // prev node does not have tail node as its next node
+                // TODO: make sure deletion will fix this
+                continue;
+            }
+            curr_node.prev.store(prev.with_tag(EXISTED_TAG), Relaxed);
+            curr_node.next.store(next.with_tag(EXISTED_TAG), Relaxed);
+            if prev_node
+                .next
+                .compare_exchange(
+                    next.with_tag(EXISTED_TAG),
+                    curr.with_tag(EXISTED_TAG),
+                    AcqRel,
+                    Acquire,
+                    &guard,
+                )
                 .is_ok()
             {
                 break;
             }
             backoff.spin();
         }
+        fence(SeqCst); // I
         Self::insert_next_ops(&curr, &next, &guard, &backoff)
     }
 
@@ -98,27 +139,102 @@ impl<T: Clone> Deque<T> {
                         Acquire,
                         guard,
                     )
-                    .is_ok() 
-                    // Changed the prev pointer of next node to the node_ptr
+                    .is_ok()
+                // Changed the prev pointer of next node to the node_ptr
                 {
-                    if node.prev
+                    // II
+                    // TODO: Analyze scenarios when current node is removed
+                    // if node.prev.load(Acquire, guard).tag() == DELETED_TAG {
+                    //     // Current node is removed
+                    // }
                 }
                 backoff.spin();
             }
         }
     }
 
-    fn mark_prev_deleted<'a>(node_ptr: &'a Shared<Node<T>>, guard: &'a Guard) {
+    pub fn remove_front<'a>(&self, guard: &'a Guard) -> Option<Shared<'a, Node<T>>> {
+        let backoff = crossbeam_utils::Backoff::new();
+        let prev = self.head.load(Acquire, guard);
+        let prev_node = unsafe { prev.deref() };
         loop {
-            unsafe {
-                let node = node_ptr.deref();
-                let node_prev = node.prev.load(Acquire, guard);
-                if node_prev.tag() == DELETED_TAG
+            let curr = prev_node.next.load(Acquire, guard);
+            if curr == self.tail.load(Relaxed, guard) {
+                // End of list
+                return None;
+            }
+            let curr_node = unsafe { curr.deref() };
+            let curr_next = curr_node.next.load(Acquire, guard);
+            if curr_next.tag() == DELETED_TAG {
+                // TODO: Make sure current node is properly removed
+                backoff.spin();
+                continue;
+            }
+            if curr_node
+                .next
+                .compare_exchange(
+                    curr_next,
+                    curr_next.with_tag(DELETED_TAG),
+                    AcqRel,
+                    Acquire,
+                    guard,
+                )
+                .is_ok()
+            {
+                // I
+                Self::unlink_node(&curr, guard, &backoff);
+                return Some(curr);
+            }
+        }
+    }
+
+    pub fn remove_back<'a>(&self, guard: &'a Guard) -> Option<Shared<'a, Node<T>>> {
+        let backoff = crossbeam_utils::Backoff::new();
+        let next_ptr = self.tail.load(Relaxed, guard);
+        let next = unsafe { next_ptr.deref() };
+        loop {
+            let node_ptr = next.prev.load(Acquire, guard);
+            let node = unsafe { node_ptr.deref() };
+            if node.next.load(Acquire, guard) != next_ptr.with_tag(EXISTED_TAG) {
+                Self::link_prev(&node_ptr, &next_ptr, guard, &backoff);
+                continue;
+            }
+            if node_ptr == self.head.load(Relaxed, guard) {
+                return None;
+            }
+            if node
+                .next
+                .compare_exchange(
+                    next_ptr.with_tag(EXISTED_TAG),
+                    next_ptr.with_tag(DELETED_TAG),
+                    AcqRel,
+                    Acquire,
+                    guard,
+                )
+                .is_ok()
+            {
+                Self::unlink_node(&node_ptr, guard, &backoff);
+                return Some(node_ptr.clone());
+            }
+            backoff.spin();
+        }
+    }
+
+    fn mark_prev_deleted<'a>(
+        node_ptr: &'a Shared<'a, Node<T>>,
+        guard: &'a Guard,
+        backoff: &Backoff,
+    ) {
+        unsafe {
+            let node = node_ptr.deref();
+            loop {
+                let node_prev_ptr = node.prev.load(Acquire, guard);
+                if node_prev_ptr.tag() == DELETED_TAG
                     || node
                         .prev
                         .compare_exchange(
-                            node_prev,
-                            node_prev.with_tag(DELETED_TAG),
+                            node_prev_ptr,
+                            node_prev_ptr.with_tag(DELETED_TAG),
                             AcqRel,
                             Acquire,
                             guard,
@@ -127,21 +243,98 @@ impl<T: Clone> Deque<T> {
                 {
                     break;
                 }
+                backoff.spin();
             }
         }
     }
 
-    fn connect_prev<'a>(prev_ptr: &Shared<'a, Node<T>>, node_ptr: &Shared<'a, Node<T>>, guard: &'a Guard) -> Shared<'a, Node<T>> {
-        unsafe {
-            let mut last = Shared::null();
-            let prev = prev_ptr.deref();
-            loop {
-                let prev_next_ptr = prev.next.load(Acquire, guard);
-                if prev_next_ptr.is_null() {
-                    if !last.is_null() {
-                        Self::mark_prev_deleted(prev_ptr, guard);
+    fn link_prev<'a>(
+        prev_ptr: &Shared<'a, Node<T>>,
+        curr_ptr: &Shared<'a, Node<T>>,
+        guard: &'a Guard,
+        backoff: &Backoff,
+    ) {
+        let mut prev_ptr = prev_ptr.clone();
+        loop {
+            let prev = unsafe { prev_ptr.deref() };
+            let prev_next_ptr = prev.next.load(Acquire, guard);
+            if prev_next_ptr.tag() == DELETED_TAG {
+                prev_ptr = prev.prev.load(Acquire, guard);
+                continue;
+            }
+            if let Err(other_prev_next) = prev.next.compare_exchange(
+                prev_next_ptr,
+                curr_ptr.with_tag(EXISTED_TAG),
+                AcqRel,
+                Acquire,
+                guard,
+            ) {
+                prev_ptr = other_prev_next.current;
+                continue;
+            }
+            break;
+        }
+    }
 
+    fn unlink_node<'a>(node_ptr: &Shared<'a, Node<T>>, guard: &'a Guard, backoff: &Backoff) {
+        fence(SeqCst);
+        Self::mark_prev_deleted(node_ptr, guard, backoff); // II
+        fence(SeqCst);
+        unsafe {
+            let node = node_ptr.deref();
+            let mut prev_ptr = node.prev.load(Acquire, guard);
+            let mut next_ptr = node.next.load(Acquire, guard);
+            loop {
+                let prev = prev_ptr.deref();
+                let next = next_ptr.deref();
+                let next_prev_ptr = next.prev.load(Acquire, guard);
+                let next_next_ptr = next.next.load(Acquire, guard);
+                if next_next_ptr.tag() == DELETED_TAG {
+                    // the next node we are going to work on is also deleted
+                    // move on to the next of the next node
+                    next_ptr = next_next_ptr;
+                    backoff.spin();
+                    continue;
+                }
+                debug_assert_eq!(next_prev_ptr.tag(), EXISTED_TAG);
+                let prev_prev_ptr = prev.prev.load(Acquire, guard);
+                let prev_next_ptr = prev.next.load(Acquire, guard);
+                if prev_next_ptr.tag() == DELETED_TAG {
+                    prev_ptr = prev_prev_ptr;
+                    backoff.spin();
+                    continue;
+                }
+                debug_assert_eq!(prev_prev_ptr.tag(), EXISTED_TAG);
+                if let Err(new_prev_next) = prev.next.compare_exchange(
+                    prev_next_ptr,
+                    next_ptr.with_tag(EXISTED_TAG),
+                    AcqRel,
+                    Acquire,
+                    guard,
+                ) {
+                    if new_prev_next.current.tag() == EXISTED_TAG {
+                        // prev next have to been changed to other node
+                        // Move prev to that node next to current prev
+                        prev_ptr = new_prev_next.current;
+                    } else {
+                        // prev have been deleted
+                        prev_ptr = prev_prev_ptr;
                     }
+                    backoff.spin();
+                    continue;
+                } // III
+                if next
+                    .prev
+                    .compare_exchange(
+                        next_prev_ptr,
+                        prev_ptr.with_tag(EXISTED_TAG),
+                        AcqRel,
+                        Release,
+                        guard,
+                    )
+                    .is_ok()
+                {
+                    break;
                 }
             }
         }
