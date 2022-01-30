28130da095b46aa0a828cf9de7ca68f311f307d9
Hao Shi <shisoftgenius@gmail.com>
2022-01-29 22:46:34 -0500

    adjust fetch and evict fn

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -3,33 +3,27 @@ use std::hash::Hash;
 
 pub struct LRUCache<K: Clone + Hash + Eq + Default, V: Clone + Default, const N: usize> {
     map: LinkedHashMap<K, V, N>,
-    capacity: usize,
-    fetch_fn: Box<dyn Fn(&K) -> Option<V>>,
-    evict_fn: Box<dyn Fn(K, V)>,
+    capacity: usize
 }
 
 impl <K: Clone + Hash + Eq + Default, V: Clone + Default, const N: usize> LRUCache<K, V, N> {
     pub fn new<FF: Fn(&K) -> Option<V> + 'static, EF: Fn(K, V) + 'static>(
         capacity: usize,
-        fetch_fn: FF,
-        evict_fn: EF,
     ) -> LRUCache<K, V, N> {
         Self {
             map: LinkedHashMap::with_capacity(capacity),
-            fetch_fn: Box::new(fetch_fn),
-            evict_fn: Box::new(evict_fn),
             capacity,
         }
     }
 
-    pub fn get(&self, key: &K) -> Option<V> {
+    pub fn get<FF: Fn(&K) -> Option<V>, EF: Fn(K, V)>(&self, key: &K, fetch_fn: FF, evict_fn: EF) -> Option<V> {
         let res = self.map.get_to_front(key);
         if res.is_none() {
-            if let Some(v) = (self.fetch_fn)(key) {
+            if let Some(v) = fetch_fn(key) {
                 self.map.insert_front(key.clone(), v.clone());
                 while self.map.len() > self.capacity {
                     if let Some(evic) = self.map.pop_back() {
-                        (self.evict_fn)(evic.0, evic.1)
+                        evict_fn(evic.0, evic.1)
                     }
                 }
                 return Some(v);
