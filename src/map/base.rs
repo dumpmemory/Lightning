a8de33c2f80cc2c89695c5d82f44bafe87a7408b
Hao Shi <shisoftgenius@gmail.com>
2023-02-18 00:28:16 +0000

    recover partition

--- src/map/base.rs
+++ src/map/base.rs
@@ -352,7 +352,7 @@ impl<
     }
 
     fn init_part_nums() -> usize {
-        1 //num_cpus::get_physical().next_power_of_two()
+        num_cpus::get_physical().next_power_of_two()
     }
 
     pub fn with_max_capacity(
